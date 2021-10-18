6c1f8709a5aa37fcb5fe988552620f3844ff3d45
saurabhbhoir11 <91054017+saurabhbhoir11@users.noreply.github.com>
2021-10-18 12:52:40 +0530

    tried highlight

---
+++ app/src/main/java/com/example/pocketide/highlighter/text_highlighter.java
@@ -0,0 +1,20 @@
+package com.example.pocketide.highlighter;
+
+import android.os.Bundle;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.pocketide.R;
+
+public class text_highlighter extends AppCompatActivity {
+   public EditText edittext;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
+
