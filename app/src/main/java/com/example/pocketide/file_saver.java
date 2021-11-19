d4a0678b134a3be608b805a0de480817bc9e4856
saurabhbhoir11 <91054017+saurabhbhoir11@users.noreply.github.com>
2021-11-19 12:20:18 +0530

    button decleration

--- app/src/main/java/com/example/pocketide/file_saver.java
+++ app/src/main/java/com/example/pocketide/file_saver.java
@@ -5,6 +5,9 @@ import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -26,6 +29,16 @@ public class file_saver extends AppCompatActivity {
                 dialog.setContentView(R.layout.save_file);
                 dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
                 dialog.setCancelable(false);
+                EditText file_name;
+                Button cancel, save;
+                RadioButton c,cpp,java;
+                file_name = findViewById(R.id.txt_input);
+                cancel = findViewById(R.id.btn_cancel);
+                save = findViewById(R.id.btn_okay);
+                c = findViewById(R.id.cprogram);
+                cpp = findViewById(R.id.cpp);
+                file_name = findViewById(R.id.txt_input);
+
                 dialog.show();
             }
         });
